cc86676e0a98ccd0de9c03385f01b459d10d26dc
RawatRahul14 <rawatrahul1604@gmail.com>
2024-12-18 18:36:05 -0500

    app update

--- app.py
+++ app.py
@@ -1,21 +1,26 @@
 import streamlit as st
 from textSummarizer.pipeline.prediction import PredictionPipeline
 
-# Initialize the prediction pipeline
 pipeline = PredictionPipeline()
 
-# Streamlit App Title
 st.title("Text Summarization App")
 
-# Input Text Box
 st.header("Enter the Text You Want to Summarize")
 input_text = st.text_area("Input your text here:", height=200)
 
-# Summarize Button
 if st.button("Summarize"):
+
     if input_text.strip():
         st.subheader("Summarized Text")
-        summarized_text = pipeline.predict(input_text)
+
+        with st.spinner("Summarizing... Please wait."):
+
+            summarized_text = pipeline.predict(input_text)
+            summarized_text = summarized_text.replace("<n>", " ")
+
+        st.success("Text summarized successfully!")
         st.write(summarized_text)
+
     else:
+        
         st.warning("Please enter some text to summarize.")
